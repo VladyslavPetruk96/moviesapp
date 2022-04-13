5d73c4381dea6d224b75a483c90933b7b7753c4c
Vladyslav Petruk <v.petruk@viseven.com>
2022-04-13 16:04:42 +0300

    apikey+axios+bootstrap init

---
+++ src/store/modules/movies.js
@@ -0,0 +1,35 @@
+import IDs from '@/store/mock/imdb_top250';
+import axios from "@/plugins/axios";
+
+const moviesStore = {
+    namespaced: true,
+    state: {
+        top250IDs: IDs,
+        moviesPerPage: 12,
+        currentPage: 1
+    },
+    getters: {
+        slicedIDs: ({ top250IDs }) => (from, to) => top250IDs.slice(from, to),
+        currentPage: ({ currentPage }) => currentPage,
+        moviesPerPage: ({ moviesPerPage }) => moviesPerPage
+    },
+    mutations: {},
+    actions: {
+        async fetchMovies({ getters }) {
+            const { currentPage, moviesPerPage, slicedIDs } = getters;
+            const from = (currentPage * moviesPerPage) - moviesPerPage;
+            const to = (currentPage * moviesPerPage);
+            const moviesToFetch = slicedIDs(from, to);
+            console.log(moviesToFetch);
+            //tt0111161
+            const response = await axios.get('/', {
+                params: {
+                    i: 'tt0111161'
+                }
+            });
+            console.log(response)
+        }
+    }
+}
+
+export default moviesStore;
