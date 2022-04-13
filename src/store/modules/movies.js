da0be153add29c7ebace1519da648d5e1596efdc
Vladyslav Petruk <v.petruk@viseven.com>
2022-04-13 18:05:20 +0300

    upd mutation, actions

--- src/store/modules/movies.js
+++ src/store/modules/movies.js
@@ -1,33 +1,48 @@
 import IDs from '@/store/mock/imdb_top250';
 import axios from "@/plugins/axios";
+import mutations from '@/store/mutations';
+
+function serializeRespons(movies) {
+    return movies.reduce((acc, movie) => {
+        acc[movie.imdbID] = movie;
+        return acc;
+    }, {});
+}
+
+const { MOVIES } = mutations;
 
 const moviesStore = {
     namespaced: true,
     state: {
         top250IDs: IDs,
         moviesPerPage: 12,
-        currentPage: 1
+        currentPage: 1,
+        movies: {}
     },
     getters: {
         slicedIDs: ({ top250IDs }) => (from, to) => top250IDs.slice(from, to),
         currentPage: ({ currentPage }) => currentPage,
         moviesPerPage: ({ moviesPerPage }) => moviesPerPage
     },
-    mutations: {},
+    mutations: {
+        [MOVIES](state, value) {
+            state.movies = value
+        }
+    },
     actions: {
-        async fetchMovies({ getters }) {
-            const { currentPage, moviesPerPage, slicedIDs } = getters;
-            const from = (currentPage * moviesPerPage) - moviesPerPage;
-            const to = (currentPage * moviesPerPage);
-            const moviesToFetch = slicedIDs(from, to);
-            console.log(moviesToFetch);
-            //tt0111161
-            const response = await axios.get('/', {
-                params: {
-                    i: 'tt0111161'
-                }
-            });
-            console.log(response)
+        async fetchMovies({ getters, commit }) {
+            try {
+                const { currentPage, moviesPerPage, slicedIDs } = getters;
+                const from = (currentPage * moviesPerPage) - moviesPerPage;
+                const to = (currentPage * moviesPerPage);
+                const moviesToFetch = slicedIDs(from, to);
+                const requests = moviesToFetch.map(id => axios.get(`/?i=${id}`));
+                const response = await Promise.all(requests);
+                const movies = serializeRespons(response);
+                commit(MOVIES, movies);
+            } catch (err) {
+                console.log(err);
+            }
         }
     }
 }
